807d1f66aa54c3053c949b7e51db8b8c23a88fd0
Tukur S Gama <Tukur S Gama@DESKTOP-592AMNP>
2023-09-25 18:11:48 +0100

    Added ludo project

---
+++ script.js
@@ -0,0 +1,11 @@
+//DIPO
+
+//Dicleartion
+
+var img = document.querySelectorAll("img") 
+var Display = document.querySelectorAll(".display") 
+var Rand = Math.random()*6 + 1
+var WholeNumb = Math.floor(Rand)
+var count = 0
+
+ 
